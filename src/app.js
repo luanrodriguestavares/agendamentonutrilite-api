f406da09ca2ce5217c3b381d8acdbe7c1184cb1e
Luan Rodrigues Tavares <luanrodrigues.contatoprofissional@outlook.com>
2025-07-24 20:40:42 -0300

    att app.js

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ const initializeApp = async () => {
 }
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`)
     initializeApp()
 })
